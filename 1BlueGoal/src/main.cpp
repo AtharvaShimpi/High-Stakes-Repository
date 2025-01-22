a02d73a6261ef6909ecfe41441668207f6b63963
AtharvaShimpi <atharva.a.shimpi@gmail.com>
2025-01-21 17:59:47 -0800

    save
    e

--- 1BlueGoal/src/main.cpp
+++ 1BlueGoal/src/main.cpp
@@ -23,11 +23,16 @@ controller Controller1;
 motor L1 = motor(PORT1,ratio6_1,true);
 motor L2 = motor(PORT2,ratio6_1,true);
 motor L3 = motor(PORT3,ratio6_1,true);
-motor R1 = motor(PORT10,ratio6_1,false);
+motor R1 = motor(PORT6,ratio6_1,false);
 motor R2 = motor(PORT9,ratio6_1,false);
 motor R3 = motor(PORT7,ratio6_1,false);
 motor Intake = motor(PORT6,ratio6_1,true);
+//motor LadyBrown = motor(PORT20,ratio6_1,true);
+//rotation Rotation = rotation(PORT1,false);
 int autonum = 1;
+
+
+
 // define your global instances of motors and other devices here
 
 /*---------------------------------------------------------------------------*/
@@ -45,25 +50,6 @@ void pre_auton(void) {
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
-void driveByDegrees(int target,int leftPower, int rightPower){
-  L1.resetPosition();
-  R1.resetPosition();
- while(abs(L1.position(deg)) < abs(target) && abs(R1.position(deg)) < abs(target)){
-  R1.spin(fwd,rightPower,vex::voltageUnits::mV);
-  R2.spin(fwd,rightPower,vex::voltageUnits::mV);
-  R3.spin(fwd,rightPower,vex::voltageUnits::mV);
-  L1.spin(fwd,leftPower,vex::voltageUnits::mV);
-  L2.spin(fwd,leftPower,vex::voltageUnits::mV);
-  L3.spin(fwd,leftPower,vex::voltageUnits::mV);
- }
-  R1.spin(fwd,0,vex::voltageUnits::mV);
-  R2.spin(fwd,0,vex::voltageUnits::mV);
-  R3.spin(fwd,0,vex::voltageUnits::mV);
-  L1.spin(fwd,0,vex::voltageUnits::mV);
-  L2.spin(fwd,0,vex::voltageUnits::mV);
-  L3.spin(fwd,0,vex::voltageUnits::mV);
-  wait(100,msec);
-}
 void drive_P(int target,int minimumSpeed)
 {
   float rightpower = 0;
@@ -72,21 +58,14 @@ void drive_P(int target,int minimumSpeed)
   bool hasCompleted = true;
   L1.resetPosition(); 
   R1.resetPosition();
-  L1.stop(coast);
-  L2.stop(coast);
-  L3.stop(coast);
-  R1.stop(coast);
-  R2.stop(coast);
-  R3.stop(coast);
   while(hasCompleted)
   {
     float lefterror = (target - L1.position(deg));
-    
     float righterror = (target - R1.position(deg));
         
     leftpower = ((100*lefterror)/abs(target));
     rightpower = ((100*righterror)/abs(target));
-    if(abs(leftpower) < minimumSpeed && abs(rightpower) < minimumSpeed) 
+    if(abs(leftpower) < minimumSpeed || abs(rightpower) < minimumSpeed) 
     { 
       if(leftpower > 0 && rightpower > 0)
       {
@@ -104,16 +83,10 @@ void drive_P(int target,int minimumSpeed)
     R1.spin(fwd,120*rightpower,vex::voltageUnits::mV);
     R2.spin(fwd,120*rightpower,vex::voltageUnits::mV);
     R3.spin(fwd,120*rightpower,vex::voltageUnits::mV);
-    if (L1.velocity(pct) == 0 && R1.velocity(pct) == 0) { 
-      velocity_timer+=10;
-    }
-    else {
-      velocity_timer = 0;
-    }
-    if(abs(lefterror) < 20 && abs(righterror) < 20) {
+    if(abs(lefterror) < 40 && abs(righterror) < 40) {
       hasCompleted = false;
       vexDisplayErase();
-    }
+    } 
     wait(10, msec);
   }
   L1.stop(hold);
@@ -122,7 +95,39 @@ void drive_P(int target,int minimumSpeed)
   R1.stop(hold);
   R2.stop(hold);
   R3.stop(hold);
-  wait(200,msec);
+  wait(50,msec);
+}
+int sgn(float input)
+{
+  if(input < 0)
+  {
+    return -1;    
+  }
+  else if(input > 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+float inertialLimit()
+{
+  if(imu.rotation(deg) > 180)
+  {
+    float limit  = (imu.rotation(deg) - 360);
+    return limit;
+  }
+  else if(imu.rotation(deg) < -180)
+  {
+    float limit = (imu.rotation(deg) + 360);
+    return limit;
+  }
+  else
+  {
+    return imu.rotation(deg);
+  }
 }
 void turn_P(int target,int minimumSpeed)
 {
@@ -132,35 +137,28 @@ void turn_P(int target,int minimumSpeed)
   bool hasCompleted = true;
   imu.resetRotation();
   float error = target - imu.rotation(deg);
-  float kP = 100/error;
-  L1.stop(coast);
-  L2.stop(coast);
-  L3.stop(coast);
-  R1.stop(coast);
-  R2.stop(coast);
-  R3.stop(coast);
+  float kP = abs(error); 
   while(hasCompleted)
   {
-    error = target - imu.rotation(deg);
-    power = abs(kP) * error; 
+    error = target - inertialLimit();
+    power =(100/kP)  * error; 
     if(abs(power) < minimumSpeed) 
     { 
       if(power > 0)
       {
         power = minimumSpeed;
-      }else
+      }
+      else if(power < 0)
       {
         power = -minimumSpeed;
       }
     }
-    L1.spin(forward,120*power,vex::voltageUnits::mV);
-    R1.spin(reverse,120*power,vex::voltageUnits::mV);
-    L2.spin(forward,120*power,vex::voltageUnits::mV);
-    R2.spin(reverse,120*power,vex::voltageUnits::mV);
-    L3.spin(forward,120*power,vex::voltageUnits::mV);
-    R3.spin(reverse,120*power,vex::voltageUnits::mV);
-    Brain.Screen.printAt(0,20,"%f", power*120);
-   
+    L1.spin(forward,90*power,vex::voltageUnits::mV);
+    R1.spin(reverse,90*power,vex::voltageUnits::mV);
+    L2.spin(forward,90*power,vex::voltageUnits::mV);
+    R2.spin(reverse,90*power,vex::voltageUnits::mV);
+    L3.spin(forward,90*power,vex::voltageUnits::mV);
+    R3.spin(reverse,90*power,vex::voltageUnits::mV);
     if(abs(error) < 5) {
       hasCompleted = false;
       vexDisplayErase();
@@ -173,9 +171,161 @@ void turn_P(int target,int minimumSpeed)
   R1.stop(hold);
   R2.stop(hold);
   R3.stop(hold);
-  wait(200,msec);
+  wait(50,msec);
 }
+/*
+enum wallStakeStates
+{
+  Idle,
+  Loading1,
+  Loading2,
+  Scoring,
+  Reset,
+};
+struct wallStake
+{
+  wallStakeStates states;
+  bool isCompleted;
+};
+wallStake wallstake;
+*/
+
+void correction(int target,int angle, int minimumSpeed)
+{
+  float power = 0;
+  float power = 0;
+  float turn_power = 0;
+  int velocity_timer = 0;
+  float error = target - L1.position(deg);
+  bool hasCompleted = true;
+  imu.resetRotation();
+  float turn_error = angle - inertialLimit();
+  float kP = abs(turn_error); 
+  while(hasCompleted)
+  {
+    turn_error = angle - inertialLimit();
+    turn_power =(100/kP)  * turn_error; 
+    if(abs(turn_power) < minimumSpeed) 
+    { 
+      if(turn_power > 0)
+      {
+        turn_power = minimumSpeed;
+      }
+      else if(turn_power < 0)
+      {
+        turn_power = -minimumSpeed;
+      }
+
+    }
+    float error = (target - L1.position(deg));
+        
+    power = ((100*error)/abs(target));
+    if(abs(power) < minimumSpeed || abs(power) < minimumSpeed) 
+    { 
+      if(power > 0 && power > 0)
+      {
+        power = minimumSpeed;
+        power = minimumSpeed;
+      }else
+      {
+        power = -minimumSpeed;
+        power = -minimumSpeed;
+      }
+    }
+    L1.spin(forward,90*(power+turn_power),vex::voltageUnits::mV);
+    R1.spin(reverse,90*(power-turn_power),vex::voltageUnits::mV);
+    L2.spin(forward,90*(power+turn_power),vex::voltageUnits::mV);
+    R2.spin(reverse,90*(power-turn_power),vex::voltageUnits::mV);
+    L3.spin(forward,90*(power+turn_power),vex::voltageUnits::mV);
+    R3.spin(reverse,90*(power-turn_power),vex::voltageUnits::mV);
+
+  L1.stop(hold);
+  L2.stop(hold);
+  L3.stop(hold);
+  R1.stop(hold);
+  R2.stop(hold);
+  R3.stop(hold);
+  wait(50,msec);
+  }
+}
+/*
+void moveWallStake (float target, wallStake wallstake)
+{
+  int sgn;
+  if(target > LadyBrown.position(deg))
+  {
+    int sgn = 1;
+  }
+  else
+  {
+    int sgn = -1;
+  }
+  wallstake.isCompleted = false;
+  if(abs(LadyBrown.position(deg)) > target)
+  {
+    wallstake.isCompleted = true;
+  }
+  LadyBrown.spin(fwd,9600*sgn,voltageUnits::mV);
+}
+int buttonPressed(){
+  if(Controller1.ButtonDown.pressing())
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int wallStakeTask ()
+{
+  while(true)
+  {
+    switch(wallstake.states)
+    {
+      case Idle:
+
+      break;
+      case Loading1:
+        moveWallStake(200,wallstake);
+        if(buttonPressed)
+        {
+          wallstake.states = Loading2;
+          wait(250,msec);
+        }
+      break;
+      case Loading2:
+        moveWallStake(300,wallstake);
+        if(buttonPressed)
+        {
+          wallstake.states = Scoring;
+          wait(250,msec);
+        }
+      break;
+      case Scoring:
+        moveWallStake(5000,wallstake);
+        if(buttonPressed)
+        {
+          wallstake.states = Reset;
+          wait(250,msec);
+        }
+      break;
+      case Reset:
+        moveWallStake(-7500,wallstake);
+        if(wallstake.isCompleted)
+        {
+          wallstake.states = Idle;
+        }
+      break;
+    }
+      wallstake.states;
+
+  }
+  return 0;
+}
+*/
 void Red1 (){
+  /*
 clamp1.set(true);
  clamp2.set(true);
  drive_P(-1600,10);
@@ -188,6 +338,8 @@ clamp1.set(true);
   wait(500,msec);
     turn_P(186,2);
     drive_P(1500,10);
+    */
+   correction(100,0,10);
 }
 void Blue1(){
   clamp1.set(true);
@@ -203,9 +355,10 @@ void Blue1(){
   turn_P(180,2);
 }
 void Red2(){
+  /*
   clamp1.set(true);
   clamp2.set(true);
-  drive_P(-1700,12);
+  drive_P(-1700,20);
   clamp1.set(false);
   clamp2.set(false);
   wait(250,msec);
@@ -221,8 +374,73 @@ void Red2(){
   drive_P(-900,15);
   turn_P(120,20);
   drive_P(2000,10);
+  */
+  drive_P(-1000,7);
+  drive_P(200,1);
+  turn_P(90,1);
+  drive_P(-400,1);
+  wait(250,msec);
+ Intake.spin(forward,100,pct);
+  wait(700,msec);
+ Intake.spin(reverse,100,pct);
+  drive_P(600,10);
+  clamp1.set(true);
+  clamp2.set(true);
+  turn_P(135,10);
+  Intake.spin(forward,100,pct);
+  drive_P(-1900,14);
+  clamp1.set(false);
+  clamp2.set(false);
+  wait(200,msec);
+  turn_P(135,10);
+  Intake.spin(forward,12000,vex::voltageUnits::mV);
+  drive_P(700,14);
+   wait(200,msec);
+  turn_P(80,10);
+  drive_P(500,14);
+   wait(200,msec);
+  drive_P(-650,14);
+  turn_P(-15,10);
+  drive_P(900,14);
+   wait(200,msec);
+  drive_P(-900,15);
+  turn_P(120,20);
+  drive_P(2000,15);
+
 }
 void Blue2(){
+  drive_P(-1000,7);
+  drive_P(200,1);
+  turn_P(-90,1);
+  drive_P(-400,1);
+  wait(250,msec);
+ Intake.spin(forward,100,pct);
+  wait(700,msec);
+ Intake.spin(reverse,100,pct);
+  drive_P(600,10);
+  clamp1.set(true);
+  clamp2.set(true);
+  turn_P(-135,10);
+  Intake.spin(forward,100,pct);
+  drive_P(-1900,14);
+  clamp1.set(false);
+  clamp2.set(false);
+  wait(200,msec);
+  turn_P(-135,10);
+  Intake.spin(forward,12000,vex::voltageUnits::mV);
+  drive_P(700,14);
+   wait(200,msec);
+  turn_P(-80,10);
+  drive_P(500,14);
+   wait(200,msec);
+  drive_P(-650,14);
+  turn_P(15,10);
+  drive_P(900,14);
+   wait(200,msec);
+  drive_P(-900,15);
+  turn_P(-120,20);
+  drive_P(2000,15);
+  /*
   clamp1.set(true);
   clamp2.set(true);
   drive_P(-1700,8);
@@ -243,6 +461,7 @@ void Blue2(){
   drive_P(-700,8);
   turn_P(-120,5);
   drive_P(2000,8);
+  */
  /*
    drive_P(-1000);
   drive_P(430);
@@ -277,7 +496,36 @@ void Blue2(){
   */
 }
 void Skills (){
-  turn_P(90,2);
+  Intake.spin(forward,12000,voltageUnits::mV);
+  wait(500,msec);
+  drive_P(650,10);
+  turn_P(90,10);
+  wait(200,msec);
+  clamp1.set(true);
+  clamp2.set(true);
+  drive_P(-1500,13);
+  clamp1.set(false);
+  clamp2.set(false);
+  wait(200,msec);
+  turn_P(180,10);
+  drive_P(1300,13);
+  wait(200,msec); 
+  drive_P(-500,10);
+  turn_P(90,10);
+  drive_P(900,10);
+  turn_P(-20,10);
+  drive_P(1100,13);
+  drive_P(-1000,13);
+  turn_P(-150,10);
+  drive_P(1500,25);
+  wait(500,msec);
+  drive_P(-1000,13);
+  turn_P(-130,10);
+  drive_P(1000,13);
+  turn_P(-10,10);
+  drive_P(-1500,20);
+
+
   /*
   Intake.spin(forward,12000,voltageUnits::mV);
   wait(500,msec);
@@ -368,8 +616,6 @@ void Skills (){
   drive_P(500);
   */
 }
-void Blue3(){
-}
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
 /*                              Autonomous Task                              */
@@ -445,7 +691,7 @@ void usercontrol(void) {
   bool prevValue1;
   bool prevValue2;
 //sets Motors to brake to enhance defensive capabilites
-
+  //vex::task wallTask(wallStakeTask);
   while (1) {
     int rightPower = Controller1.Axis2.position()*120;
     int leftPower = Controller1.Axis3.position()*120;
